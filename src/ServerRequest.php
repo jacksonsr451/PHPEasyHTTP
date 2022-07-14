6bd76dbf8e729220c956b640257e78b5d3dffc5b
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-07-13 23:18:14 -0300

    Update LICENSE

--- src/ServerRequest.php
+++ src/ServerRequest.php
@@ -10,4 +10,81 @@ class ServerRequest implements ServerRequestInterface
 {
 	use MessageTrait;
     use RequestTrait;
+
+	private array $servers;
+	private array $cookies;
+	private array $queries;
+
+	public function getServerParams(): array
+	{
+		return $this->servers;
+	}
+
+	public function getCookieParams(): array
+	{
+		return $this->cookies;
+	}
+
+	public function withCookieParams(array $cookies): self
+	{
+		$clone = clone $this;
+		$clone->cookies = $cookies;
+		return $clone;
+	}
+
+	public function getQueryParams(): array
+	{
+		if (! empty($this->queries)) return $this->queries;
+		$queries = [];
+		parse_str($this->getUri()->getQuery(), $queries);
+		return $queries;
+	}
+
+	public function withQueryParams(array $query): self
+	{
+		$clone = clone $this;
+		$clone->queries = $query;
+		return $clone;
+	}
+
+	public function getUploadedFiles(): array
+	{
+		
+	}
+
+	public function withUploadedFiles(array $uploadedFiles): ServerRequestInterface
+	{
+		
+	}
+
+	public function getParsedBody(): null|array|object
+	{
+		if ($this->inPost()) return $_POST;
+		return $this->body;
+	}
+
+	public function inPost(): bool
+	{
+		$postHeaders = ['application/x-www-form-urlencoded', 'multpart/form-data'];
+		$headersValues = $this->getHeader('content-type');
+		foreach ($headersValues as $value) {
+			if (in_array($value, $postHeaders)) return true;
+		}
+		return false;
+	}
+
+	public function withParsedBody($data): ServerRequestInterface
+	{
+		
+	}
+
+	public function getAttributes(): mixed
+	{
+		
+	}
+
+	public function getAttribute($name, $default = null): mixed
+	{
+		
+	}
 }
