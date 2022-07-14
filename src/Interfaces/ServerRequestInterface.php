541571a74863bfa7a884fec9502325a69cff1d38
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-07-13 22:08:16 -0300

    Add class ServerRequest

--- src/Interfaces/ServerRequestInterface.php
+++ src/Interfaces/ServerRequestInterface.php
@@ -2,31 +2,31 @@
 
 namespace PhpEasyHttp\Http\Message\Interfaces;
 
-interface ServerRequestInterface 
+interface ServerRequestInterface extends RequestInterface
 {
     public function getServerParams(): array;
 
     public function getCookieParams(): array;
 
-    public function withCookieParams(array $cookies): static;
+    public function withCookieParams(array $cookies): self;
 
     public function getQueryParams(): array;
 
-    public function withQueryParams(array $query): static;
+    public function withQueryParams(array $query): self;
 
     public function getUploadedFiles(): array;
 
-    public function withUploadedFiles(array $uploadedFiles): static;
+    public function withUploadedFiles(array $uploadedFiles): self;
 
     public function getParsedBody(): null|array|object;
 
-    public function withParsedBody($data): static;
+    public function withParsedBody($data): self;
 
     public function getAttributes(): mixed;
 
     public function getAttribute($name, $default = null): mixed;
 
-    public function withAttribute($name, $value): static;
+    public function withAttribute($name, $value): self;
 
-    public function withoutAttribute($name): static;
+    public function withoutAttribute($name): self;
 }
