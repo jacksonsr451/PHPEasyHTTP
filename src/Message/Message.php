fa6247128d29a44337995997b69cabbe5bd3715b
Jackson Severino da Rocha <jackson.severino.rocha@escola.pr.gov.br>
2022-07-16 09:49:34 -0300

    Add classes by message

---
+++ src/Message/Message.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PhpEasyHttp\Http\Message;
+
+use PhpEasyHttp\Http\Message\Interfaces\MessageInterface;
+use PhpEasyHttp\HTTP\Message\Traits\MessageTrait;
+
+class Message implements MessageInterface
+{
+    use MessageTrait;
+}
